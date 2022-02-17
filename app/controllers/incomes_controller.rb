9a5304ee3fee0ad52181019391fc597e1ff8f27d
ishii-0607 <96577145+ishii-0607@users.noreply.github.com>
2022-02-17 04:49:06 +0000

    [Finish]incomes_index

---
+++ app/controllers/incomes_controller.rb
@@ -0,0 +1,25 @@
+class IncomesController < ApplicationController
+
+  def index
+    @incomes = Income.all.order(created_at: :asc)
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
